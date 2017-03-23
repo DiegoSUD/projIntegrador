4e81b96568715e3c1311ed8b92dd1c7ce0f1f40c
Diego Pereira <diegosud.pereira@gmail.com>
2017-03-23 15:56:09 -0300

    modificado

--- src/pinteg1/saudação.java
+++ src/pinteg1/saudação.java
@@ -3,26 +3,20 @@ package pinteg1;
 import java.util.Scanner;
 
 public class saudação {
+
     public static void main(String[] args) {
-            Scanner leia = new Scanner(System.in);
-            String nome;
-            System.out.println("Escreva o nome da pessoa : ");
-            nome = leia.next();
-            // Não se pode colocar == "" ele reconhece ( coloca-se nome.equals)
-            // para inverter a ordem colocase (!nome.equals (" "))
-            
-            
-            if (nome.equals (" ")){
-                System.out.println("Olá, mundo!");
-            } else {
-                System.out.println("Óla, " + nome + "!");
-            }
-         }
-            
-            
-            
-        
-        
+        Scanner leia = new Scanner(System.in);
+        String nome;
+        System.out.println("Escreva o nome da pessoa : ");
+        nome = leia.next();
+        // Não se pode colocar == "" ele reconhece ( coloca-se nome.equals)
+        // para inverter a ordem colocase (!nome.equals (" "))
+
+        if (nome.equals(" ")) {
+            System.out.println("Olá, mundo!");
+        } else {
+            System.out.println("Óla, " + nome + "!");
+        }
     }
-    
+
 }
